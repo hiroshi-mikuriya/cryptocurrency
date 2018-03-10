3f8b12ec594c0a463653d71971fe71050c498423
hiroshi <hiroshi@iMacHiroshi2.local>
2018-03-11 00:10:20 +0900

    mod cryptobridge time

--- combine/result/self_calc.rb
+++ combine/result/self_calc.rb
@@ -7,12 +7,14 @@ csv.each do |c|
   # Date,Dealer,Exchange1,Exchange2,Type,Quantity,Limit,CommissionPaid,Price
   # 2017/07/29,bitFlyer,JPY,ETH,BUY,2.1,23760,,49896
   # 2017/08/19,zaif,BTC,XEM,SELL,750,0.00005805,0.00004354,0.04358104
+  ex1, ex2, price, quan = %w[Exchange1 Exchange2 Price Quantity].map { |k| c[k] }
+  p [price, quan] if [ex1, ex2].any? {|ex| ex=='ZNY'}
   if c['Type'] == 'BUY'
-    o[c['Exchange1']] -= c['Price'].to_f
-    o[c['Exchange2']] += c['Quantity'].to_f
+    o[ex1] -= price.delete(',').to_f
+    o[ex2] += quan.delete(',').to_f
   else
-    o[c['Exchange1']] += c['Price'].to_f
-    o[c['Exchange2']] -= c['Quantity'].to_f
+    o[ex1] += price.delete(',').to_f
+    o[ex2] -= quan.delete(',').to_f
   end
   break if Time.parse('2018/1/1') <= Time.parse(c['Date'])
 end
