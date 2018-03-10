13cb37152612255486bd0322980d6a1de38c1628
御厨 洋 <mickly9@MacBookAirHiroshi.local>
2018-03-10 15:00:43 +0900

    all deposit withdrawal

--- combine/combine.rb
+++ combine/combine.rb
@@ -4,7 +4,8 @@ require 'time'
 files = %w[
   other_depo_with.csv
   zaif_depo_with.csv
-  coincheck_depo_with
+  coincheck_depo_with.csv
+  bitflyer_depo_with.csv
 ]
 
 data = files.each.with_object([]) do |f, o|
@@ -12,6 +13,9 @@ data = files.each.with_object([]) do |f, o|
 end
 
 data.sort_by! {|a| Time.parse(a['DATE']) }
+data.size.times do |i|
+  data[i]['DATE'] = Time.parse(data[i]['DATE']).strftime("%Y/%m/%d")
+end
 puts CSV.generate('', headers: %w[DATE CURRENCY UNITS FEE FROM TO], write_headers: true) { |csv|
   data.each { |d| csv << d }
 }
